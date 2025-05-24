e79d3048d2fdd063ac30ea2970013e18a8e74dbc
Guilherme Costa <costa.gguilherme02@gmail.com>
2025-05-24 13:29:41 -0300

    Scripts Vitima e Atancate

---
+++ atacante_h2.py
@@ -0,0 +1,17 @@
+import socket
+
+HOST = '10.0.1.1'  # IP da vítima
+PORT = 1234
+
+mensagem = ("Prezado(a) cliente, identificamos uma pendência financeira em seu CPF no valor de R$289,70.\n\n"
+            "Para evitar negativação do nome, acesse imediatamente o portal abaixo e realize a verificação dos dados:\n"
+            "consulta-serasa.com:8080/login\n\n"
+            "Após o login, você poderá gerar o boleto de quitação ou parcelar sua dívida.\n\n"
+            "Atendimento automático 24h.")
+
+tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+tcp.connect((HOST, PORT))
+tcp.sendall(mensagem.encode())
+print("[ATACANTE] Mensagem de Phishing enviada com sucesso.")
+tcp.close()
---
+++ vitima_h1.py
@@ -0,0 +1,19 @@
+HOST = '0.0.0.0'
+PORT = 1234
+
+tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+tcp.bind((HOST, PORT))
+tcp.listen()
+print("[VÍTIMA] ONLINE")
+
+conn, addr = tcp.accept()
+
+data = conn.recv(1024)
+if data:
+    print(f"[Serasa]: {data.decode()}")
+    with open("chat.txt", "a") as f:
+        f.write(f"{data.decode()}\n")
+
+conn.close()
+tcp.close()
